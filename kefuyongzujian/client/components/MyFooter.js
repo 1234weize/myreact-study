b65dec42193555bf882d2441fa55c84eb33c6d4d
weize0 <weize@xiaogu-tech.com>
2018-08-23 18:58:58 +0800

    增加了可复用组件，组件共享，组件refs

---
+++ kefuyongzujian/client/components/MyFooter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class MyFooter extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>这是底部，存放版权信息</h1>
+            </div>
+        )
+    }
+}
---
+++ kefuyongzujian/client/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyHeader from './components/MyHeader';
+import MyFooter from './components/MyFooter';
+import MyBody from './components/MyBody';
+
+
+class Index extends React.Component{
+    render(){
+        return(
+            <div>
+                <MyHeader />
+                <MyBody userid={123456} username={"nick"} />
+                <MyFooter />
+            </div>
+        )
+    }
+}
+ReactDOM.render(<Index/>, document.getElementById('example'));
