fb62f37aa66a1000e39e75795d4e63cffe5b7792
weize0 <weize@xiaogu-tech.com>
2018-08-24 22:18:47 +0800

    CSS模块化

--- 0901/client/components/MyFooter.js
+++ 0901/client/components/MyFooter.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import miniFooter from '../css/footer.css';
 
+import { miniFooter } from "../css/footer.css";
 
 export default class MyFooter extends React.Component{
     render(){
+        // console.log(footerCss);
+        console.log(123);
         return (
-            <div>
+            <footer className="miniFooter">
                 <h1>这是底部，存放版权信息</h1>
-            </div>
+            </footer>
         )
     }
 }
