0cad494708e2e25e563dd972d0f5a618af22e0d3
weize0 <weize@xiaogu-tech.com>
2018-08-24 09:05:43 +0800

    内联样式中的表达式

--- 0901/client/components/MyHeader.js
+++ 0901/client/components/MyHeader.js
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom';
 
 
 export default class MyHeader extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            miniHeader:false//默认加载还是高的头部
+        }
+    };
+    switchHeader(){
+        this.setState({
+            miniHeader:!this.state.miniHeader
+        });
+    };
     render(){
         const styleComponentHeader = {
             header:{
                 backgroundColor:"#333333",
                 color:"#FFFFFF",
-                paddingTop: "15px",
-                paddingBottom: "15px"
+                paddingTop: (this.state.miniHeader)?"3px":"15px",
+                paddingBottom: (this.state.miniHeader)?"3px":"15px",
             },
           //还可以定义其他的样式  
         };
         return (
-            <header style={styleComponentHeader.header} className="smallFontSize">
+            <header style={styleComponentHeader.header} className="smallFontSize"
+            onClick={this.switchHeader.bind(this)}>
                 <h1>这是头部</h1>
             </header>
         )
